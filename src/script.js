3d46c43c7e8ea898039eaec29b4a24b28fbc3453
JesusPastorini <juniorpastorini_@hotmail.com>
2023-03-21 04:49:42 -0300

    termino da logica principal

--- src/script.js
+++ src/script.js
@@ -2,16 +2,30 @@ import './style.css';
 
 const coin = document.querySelector('#coin');
 const botao = document.querySelector('#botao');
-const moedaDevolve = document.querySelector('.coinDevolve');
+const moedaTitulo = document.querySelector('.coinTitulo');
 const listaMoeda = document.querySelector('.listaCoin');
 
 const executaClick = () => {
 const moeda = coin.value;
 const url = `https://api.exchangerate.host/latest?base=${moeda}`;
-
+console.log(url);
     fetch(url)
         .then((res) => res.json())
-        .then((data) => console.log(data))  
-
+        .then((data) => renderisaRates(data.rates))  
+    moedaTitulo.innerHTML = `Valores referente a 1 ${moeda.toUpperCase()}`;
 };
 botao.addEventListener('click', (executaClick));
+
+const renderisaRates = (ratess) => {
+    listaMoeda.innerHTML = '';
+const ratesArray = Object.entries(ratess);
+console.log(ratesArray);
+
+ratesArray.forEach((rate) => {
+    const [coinName, valor] = rate;
+
+    const li = document.createElement('li');
+    li.innerHTML = `${coinName} - ${valor}`;
+    listaMoeda.appendChild(li);
+});
+};
